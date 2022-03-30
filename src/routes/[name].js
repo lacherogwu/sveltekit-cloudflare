d3e60c14a9a24a74eb4c73937398586cb29c9960
lachero <asaf@utopia.tech>
2022-03-30 20:27:17 +0300

    added requests

--- src/routes/[name].js
+++ src/routes/[name].js
@@ -1,9 +1,21 @@
 export const get = async (request) => {
 	const { name } = request.params;
 
+	const apiUrl = 'https://webhook.site/bfdd2cfe-78be-483e-ae2e-e3e20ddaaff6';
+	const response = await fetch(apiUrl, {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json'
+		},
+		body: JSON.stringify({ message: `Welcome ${name}, he just logged in to your website!` })
+	});
+
+	const json = await response.json();
+
 	return {
 		body: {
-			name
+			name,
+			json
 		}
 	};
 };
--- src/routes/[name].svelte
+++ src/routes/[name].svelte
@@ -1,8 +1,27 @@
 <script>
 	export let name;
+	export let json = {};
+
+	const handleClick = async () => {
+		const apiUrl = 'https://webhook.site/bfdd2cfe-78be-483e-ae2e-e3e20ddaaff6';
+		const response = await fetch(apiUrl, {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify({ message: `${name}, Just clicked THE button!` })
+		});
+
+		json = await response.json();
+	};
 </script>
 
 <svelte:head>
 	<title>Saying Hello to everyone! 🙋‍♂️</title>
 </svelte:head>
 <h1>Hello {name}</h1>
+<button on:click={handleClick}>Click Me</button>
+<div>
+	<h1>Response:</h1>
+	<div>{JSON.stringify(json)}</div>
+</div>
